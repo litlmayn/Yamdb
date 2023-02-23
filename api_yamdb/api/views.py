d980589ec7652450bbdf0c827a0f1ee60e2b5144
Roman Romantsov <romantsov1999@mail.ru>
2023-02-23 18:39:39 +0300

    view and urls

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from .serializers import GenresSerializer, TitleSerializer, CategorieSerializer
+from titles.models import Title, Genres, Categories
+
+
+class GetListCreateDeleteViewSet(mixins.ListModelMixin,
+                                 mixins.CreateModelMixin,
+                                 mixins.DestroyModelMixin,
+                                 viewsets.GenericViewSet):
+    pass
+
+
+class GenresViewSet(GetListCreateDeleteViewSet):
+    queryset = Genres.objects.all()
+    serializer_class = GenresSerializer
+
+
+class CategoriesViewSet(GetListCreateDeleteViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategorieSerializer
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
