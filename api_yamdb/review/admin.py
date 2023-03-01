e2be6a0e934adcbd36e2ccc010f725adf1fe5620
Вадим Крамаренко <litlmayn@mail.ru>
2023-03-01 14:13:39 +0300

    сделал миграции и свою админку

--- api_yamdb/review/admin.py
+++ api_yamdb/review/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review, Comment
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'author', 'pub_date',
+        'text', 'score', 'title',
+    )
+    search_fields = 'author'
+    list_filter = 'pub_date'
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'author', 'pub_date',
+        'text', 'review',
+    )
+    search_fields = 'author'
+    list_filter = 'pub_date'
+    empty_value_display = '-пусто-'
+
+admin.site.register(Comment)
+admin.site.register(Review)
