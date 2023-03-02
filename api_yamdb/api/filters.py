ee39c50b1762e31993279edbc873b3ce6c1fe0a7
Roman Romantsov <romantsov1999@mail.ru>
2023-03-02 21:02:00 +0300

    filter

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -4,6 +4,9 @@ from titles.models import Title
 
 
 class TitleFilter(FilterSet):
-    genre = CharFilter(field_name='genres__slug',)
-    category = CharFilter(field_name='categories__slug',)
+    genre = CharFilter(field_name='genre__slug',)
+    category = CharFilter(field_name='category__slug',)
     name = CharFilter(field_name='name', lookup_expr='contains',)
+    class Meta:
+        model = Title
+        fields = ['year']
