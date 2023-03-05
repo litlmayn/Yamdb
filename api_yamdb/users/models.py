a6278a2069a6dcee2e27f9c99778f3ca482403ab
Настасья Мартынова <martynovanastasya@gmail.com>
2023-03-05 22:11:46 +0300

    User

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.conf import settings
+
 from api.validators import username_validator
 
 
